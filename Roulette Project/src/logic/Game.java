6a84e82f38fd29ba1c4fd2d615fca55db75ba8ae
pelayolartategui <pelayolartategui@gmail.com>
2015-11-04 13:01:28 +0100

    cc

--- Roulette Project/src/logic/Game.java
+++ Roulette Project/src/logic/Game.java
@@ -1,9 +1,25 @@
 package logic;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 public class Game {
+	private Player player;
 	//ARRAY PLAYERS?
-	ArrayList<Player> players;
+	//ArrayList<Player> players;
 	//Make a bet(player,amount,typeOfBet)
+
+	public Game(String playerName,Color color) {
+		player=new Player(playerName,color);
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	
+	
 }
--- Roulette Project/src/ui/MainWindow.java
+++ Roulette Project/src/ui/MainWindow.java
@@ -6,16 +6,36 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import logic.Game;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
-import javax.swing.JPopupMenu;
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+import javax.swing.JScrollBar;
 
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu mnRoulette;
+	private JMenuItem mntmNewGame;
+	private JMenuItem mntmNewPlayer;
+	private JMenuItem mntmExit;
+	private JMenu mnHelp;
+	private JMenuItem mntmAbout;
+	private JTextField tfMoney;
+	private JLabel lblPlayerName;
+	private Game game;
+	private JTextPane textPane;
+	private JScrollBar scrollBar;
 
 	/**
 	 * Launch the application.
@@ -37,35 +57,122 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
+		game=new Game("DefaultPlayer", Color.BLUE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 783, 443);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnRoulette = new JMenu("Roulette");
-		menuBar.add(mnRoulette);
+		setBounds(100, 100, 569, 437);
+		setJMenuBar(getMenuBar_1());
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(getTfMoney());
+		contentPane.add(getLblPlayerName());
+		contentPane.add(getTextPane());
+		contentPane.add(getScrollBar());
+		
+		representStatus();
 	}
 
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
+	private void representStatus() {
+		representMoney();
+		if(this.game.getPlayer().getMoney()<=0){
+			JOptionPane.showMessageDialog(null,"Game Over");
+		}
+		
+		
+	}
+
+	private void representMoney() {
+		double money = game.getPlayer().getMoney();
+		this.tfMoney.setText(String.valueOf(money));
+		
+	}
+	
+	private void playGame(){
+		
+	}
+
+	private JMenuBar getMenuBar_1() {
+		if (menuBar == null) {
+			menuBar = new JMenuBar();
+			menuBar.add(getMnRoulette());
+			menuBar.add(getMnHelp());
+		}
+		return menuBar;
+	}
+	private JMenu getMnRoulette() {
+		if (mnRoulette == null) {
+			mnRoulette = new JMenu("Roulette");
+			mnRoulette.setSelectedIcon(new ImageIcon(MainWindow.class.getResource("/img/icon175x175.png")));
+			mnRoulette.add(getMntmNewGame());
+			mnRoulette.add(getMntmNewPlayer());
+			mnRoulette.add(getMntmExit());
+		}
+		return mnRoulette;
+	}
+	private JMenuItem getMntmNewGame() {
+		if (mntmNewGame == null) {
+			mntmNewGame = new JMenuItem("New Game");
+		}
+		return mntmNewGame;
+	}
+	private JMenuItem getMntmNewPlayer() {
+		if (mntmNewPlayer == null) {
+			mntmNewPlayer = new JMenuItem("New Player");
+			mntmNewPlayer.setEnabled(false);
+		}
+		return mntmNewPlayer;
+	}
+	private JMenuItem getMntmExit() {
+		if (mntmExit == null) {
+			mntmExit = new JMenuItem("Exit");
+		}
+		return mntmExit;
+	}
+	private JMenu getMnHelp() {
+		if (mnHelp == null) {
+			mnHelp = new JMenu("Help");
+			mnHelp.add(getMntmAbout());
+		}
+		return mnHelp;
+	}
+	private JMenuItem getMntmAbout() {
+		if (mntmAbout == null) {
+			mntmAbout = new JMenuItem("About");
+		}
+		return mntmAbout;
+	}
+	private JTextField getTfMoney() {
+		if (tfMoney == null) {
+			tfMoney = new JTextField();
+			tfMoney.setEditable(false);
+			tfMoney.setHorizontalAlignment(SwingConstants.CENTER);
+			tfMoney.setBounds(323, 39, 86, 20);
+			tfMoney.setColumns(10);
+		}
+		return tfMoney;
+	}
+	private JLabel getLblPlayerName() {
+		if (lblPlayerName == null) {
+			lblPlayerName = new JLabel(game.getPlayer().getName());
+			lblPlayerName.setHorizontalAlignment(SwingConstants.CENTER);
+			lblPlayerName.setForeground(game.getPlayer().getColor());
+			lblPlayerName.setBounds(328, 24, 81, 14);
+		}
+		return lblPlayerName;
+	}
+	private JTextPane getTextPane() {
+		if (textPane == null) {
+			textPane = new JTextPane();
+			textPane.setBounds(276, 84, 133, 90);
+		}
+		return textPane;
+	}
+	private JScrollBar getScrollBar() {
+		if (scrollBar == null) {
+			scrollBar = new JScrollBar();
+			scrollBar.setBounds(392, 102, 17, 48);
+		}
+		return scrollBar;
 	}
 }
