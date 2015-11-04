5a0ad7b0d33bcd9dd8c27a80d45587f495ea7668
courentin <ch2kn@free.fr>
2015-11-04 19:09:22 +0100

    Add statistics functions

--- Roulette Project/src/logic/Game.java
+++ Roulette Project/src/logic/Game.java
@@ -2,6 +2,7 @@ package logic;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import logic.typeOfBets.Column;
@@ -34,12 +35,77 @@ public class Game {
 		player.executeBets(number);
 		return number;
 	}
-	
+
 	/*
-	public int[] getHotNumbers() {
+	public List<Number> getHotNumbers() {
+		List<Number> ordered = history;
+		for(Number number : history) {
+			
+		}
 		
+		Collections.sort(ordered);
+		
+		return ordered.subList(0, 3);
 	}
 	*/
+	
+	 /**
+	  * 
+	  * @return
+	  * 0 -> RED
+	  * 1 -> ZERO
+	  * 2 -> BLACK
+	  */
+	 public List<Double> getRedBlackStatistics() {
+		 List<Double> stat = new ArrayList<>();
+		 stat.add(0.0);
+		 stat.add(0.0);
+		 stat.add(0.0);
+		 for(Number number : history) {
+			 if(number.getColor() == Color.RED) {
+				 stat.set(0, 1+stat.get(0));
+			 } else if(number.getColor() == Color.BLACK) {
+				 stat.set(2, 1+stat.get(2));
+			 } else {
+				 stat.set(0, 1+stat.get(0));
+			 }
+		 }
+		 int size = history.size();
+		 stat.set(0, stat.get(0) / size);
+		 stat.set(1, stat.get(1) / size);
+		 stat.set(2, stat.get(2) / size);
+
+		 return stat;
+	 }
+
+	 /**
+	  * 
+	  * @return
+	  * 0 -> EVEN
+	  * 1 -> ZERO
+	  * 2 -> ODD
+	  */
+	 public List<Double> getEvenOddStatistics() {
+		 List<Double> stat = new ArrayList<>();
+		 stat.add(0.0);
+		 stat.add(0.0);
+		 stat.add(0.0);
+		 for(Number number : history) {
+			 if(number.getValue() == 0)
+				 stat.set(0, 1+stat.get(0));
+			 if(number.getValue()%2 == 0) {
+				 stat.set(0, 1+stat.get(0));
+			 } else {
+				 stat.set(2, 1+stat.get(2));
+			 }
+		 }
+		 int size = history.size();
+		 stat.set(0, stat.get(0) / size);
+		 stat.set(1, stat.get(1) / size);
+		 stat.set(2, stat.get(2) / size);
+
+		 return stat;
+	 }
 
 	private void chargeBets() {
 		betsAvailable[0]=new Column();
