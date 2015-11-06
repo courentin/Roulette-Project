440130b89b3e8f76310af25f5ca1750982901216
courentin <ch2kn@free.fr>
2015-11-06 09:45:04 +0100

    Corner bet modif

--- Roulette Project/src/logic/typeOfBets/CornerBet.java
+++ Roulette Project/src/logic/typeOfBets/CornerBet.java
@@ -1,28 +1,51 @@
 package logic.typeOfBets;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import logic.Number;
 
 public class CornerBet extends TypeOfBet {
-	private HashSet<Integer> betBoxes;
+	private List<Integer> numbers;
 
 	public CornerBet() {
 		super();
 		this.coeficient = 8;
 	}
 	
-	public void setBetBoxes(HashSet<Integer> betBoxes) {
-		this.betBoxes = betBoxes;
+	public void setBetBoxes(int one, int two, int three, int four) {
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(one);
+		numbers.add(two);
+		numbers.add(three);
+		numbers.add(four);
+		this.setBetBoxes(numbers);
+	}
+	
+	public void setBetBoxes(List<Integer> numbers) throws IllegalArgumentException {
+		Collections.sort(numbers, new Comparator<Integer>() {
+			  public int compare(Integer c1, Integer c2) {
+			    if (c1.intValue() > c2.intValue()) return -1;
+			    if (c1.intValue() < c2.intValue()) return 1;
+			    return 0;
+		}});
+		
+		if(numbers.get(0)+1 == numbers.get(1) && numbers.get(1)+2 == numbers.get(2) && numbers.get(2)+1 == numbers.get(3)) {
+			this.numbers = numbers;
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	@Override
 	public boolean isWinnerNumber(Number box) {
-		return betBoxes.contains(box.getValue());	
+		return numbers.contains(box.getValue());	
 	}
 
 	@Override
 	public String printInfo() {
-		
-		return betBoxes.toString();
+		return numbers.toString();
 	}
 }
