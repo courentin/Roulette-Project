91885847a564a44cf687ea3467f73a3f4aceba3d
courentin <ch2kn@free.fr>
2015-11-06 09:54:26 +0100

    Change sort

--- Roulette Project/src/logic/typeOfBets/CornerBet.java
+++ Roulette Project/src/logic/typeOfBets/CornerBet.java
@@ -25,13 +25,8 @@ public class CornerBet extends TypeOfBet {
 	}
 	
 	public void setBetBoxes(List<Integer> numbers) throws IllegalArgumentException {
-		Collections.sort(numbers, new Comparator<Integer>() {
-			  public int compare(Integer c1, Integer c2) {
-			    if (c1.intValue() > c2.intValue()) return -1;
-			    if (c1.intValue() < c2.intValue()) return 1;
-			    return 0;
-		}});
-		
+		Collections.sort(numbers);
+
 		if(numbers.get(0)+1 == numbers.get(1) && numbers.get(1)+2 == numbers.get(2) && numbers.get(2)+1 == numbers.get(3)) {
 			this.numbers = numbers;
 		} else {
